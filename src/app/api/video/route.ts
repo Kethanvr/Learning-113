11bc1b5e5f3aa7f49643097c0766b6db0d38c021
kethan VR <kethanvr@gmail.com>
2025-07-19 18:20:05 +0530

    refactor(video-api): enhance POST handler with session validation and error handling

--- src/app/api/video/route.ts
+++ src/app/api/video/route.ts
@@ -1,8 +1,8 @@
 import { conntodb } from "@/lib/db";
-import Video from "@/modles/Video";
-import { NextResponse } from "next/server";
-import User from "@/modles/user";
-import { error } from "console";
+import Video, { Ivideo } from "@/modles/Video";
+import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 export async function GET() {
   try {
@@ -30,18 +30,51 @@ export async function GET() {
   }
 }
 
-export async function POST() {
+export async function POST(request: NextRequest) {
   try {
-    await conntodb();
-    const user = await User.findOne().lean();
-
-    if (!User) {
+    const session = await getServerSession(authOptions);
+    if (!session) {
       return NextResponse.json(
-        { error: "user not found in db" },
-        { status: 500 }
+        { error: "user not authorised" },
+        {
+          status: 500,
+        }
       );
-
-      
     }
-  } catch (error) {}
+    await conntodb();
+
+    const body: Ivideo = await request.json();
+
+    if (body.title || body.desciption || body.videourl || body.thumbnailurl) {
+      return NextResponse.json(
+        { error: "user not authorised" },
+        {
+          status: 500,
+        }
+      );
+    }
+
+    const videodata = {
+      ...body,
+      controls: body?.controls ?? true,
+      transfortransformation: {
+        height: 1920,
+        width: 1080,
+        quality: body.transformation?.quality || 100,
+      },
+    };
+
+     const newVideo = await Video.create(videodata);
+     
+    return NextResponse.json(newVideo,{
+        status:200
+    })
+  } catch (error) {
+
+    return NextResponse.json(
+
+      { error: "user not found in db" },
+      { status: 500 }
+    ); 
+  }
 }
